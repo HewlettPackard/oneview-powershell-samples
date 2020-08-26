6ddfa6381e40f92c2afd006bbe67476e3f21b385
Chris Lynch <chris.lynch@hpe.com>
2020-08-26 10:54:03 -0700

    Updating to support newer Cmdlets, and the OneView 5.30 library naming convention of the Cmdlets
    Signed-off-by: Chris Lynch <chris.lynch@hpe.com>

--- Server Profiles/Set-and-Update-BiosSettings-in-SPT.ps1
+++ Server Profiles/Set-and-Update-BiosSettings-in-SPT.ps1
@@ -1,8 +1,8 @@
-
-
+# Updating to support newer Cmdlets, and the OneView 5.30 library naming convention of the Cmdlets
+$SPTName = 'BFS'
 
 # ----------------- Get SPT
-$spt                        = get-HPOVserverprofileTemplate -name 'BFS'
+$spt                        = Get-OVServerProfileTemplate -Name $SPTName
 
 # ------------------- BIOS Attribute
 write-host -foreground CYAN ' Enable BIOS and configure first settings......'
@@ -22,14 +22,14 @@ $currentBios.overriddenSettings     = $biosSettings
 
 
 # ----------------- Update biosSettings
-Set-HPOVResource -InputObject $spt | Wait-HPOVTaskComplete | fl *
+Save-OVServerProfileTemplate -InputObject $spt | Wait-OVTaskComplete | fl *
 
 
 # ----------------- Add second set of Bios Settins
 write-host -foreground CYAN ' Wait 5 seconds for SPT to update and then get the latest version of SPT .....'
 sleep -Seconds 5
 # ---- Refresh the sspt
-$spt                        = get-HPOVserverprofileTemplate -name 'BFS'
+$spt                        = Get-OVServerProfileTemplate -Name $SPTName
 $currentBios                = $spt.Bios
 $updatedBiosSettings        = @()
 
@@ -60,8 +60,13 @@ $currentBios.overriddenSettings = $updatedBiosSettings
 
 write-host -foreground CYAN ' Add 2nd set of BIOS settings .....'
 # ----------------- Update biosSettings
-Set-HPOVResource -InputObject $spt | Wait-HPOVTaskComplete | fl *
+Save-OVServerProfileTemplate -InputObject $spt | Wait-OVTaskComplete | fl *
 
-          
+# Get the list of Server Profiles associated, and those that are marked as NotCompliant
+Get-OVServerProfile -InputObject $SPT -NonCompliant
 
+# See what will change with the associated server profiles if an update from template operation were performed
+Get-OVServerProfile -InputObject $SPT -NonCompliant | Update-OVServerProfile -WhatIf
 
+# Now, update the associated server profiles to be consistent/compliant with the associated server profile template without confirmation and asynchronously
+$Tasks = Get-OVServerProfile -InputObject $SPT -NonCompliant | Update-OVServerProfile -Confirm:$false -Async
