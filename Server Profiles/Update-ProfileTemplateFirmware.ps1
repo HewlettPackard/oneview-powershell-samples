63b84c2b48df4c21a1906469b1e27b7bb524d1a6
Chris Lynch <chris.lynch@hpe.com>
2021-07-13 09:58:22 -0700

    updating Update-ProfileTEemplateFirmware sample script

--- Server Profiles/Update-ProfileTemplateFirmware.ps1
+++ Server Profiles/Update-ProfileTemplateFirmware.ps1
@@ -1,4 +1,4 @@
- # Copyright 2020 Hewlett Packard Enterprise Development LP
+ # Copyright 2021 Hewlett Packard Enterprise Development LP
  #
  # Licensed under the Apache License, Version 2.0 (the "License"); you may
  # not use this file except in compliance with the License. You may obtain
@@ -22,23 +22,63 @@ $template                   = "NCS ESXi 6.5 Server Profile Template"
 $newBaseline                = "HPE Synergy Custom SPP 201903 2019 06 12, 2019.06.12.00"
 
 # Get profile template
-$thisTemplate               = Get-HPOVServerProfileTemplate -name $template -ErrorAction Stop
+$thisTemplate               = Get-OVServerProfileTemplate -name $template -ErrorAction Stop
 
 if ($NULL -ne $thisTemplate)
 {
-        $thisBaseline       = Get-HPOVBaseline -SppName $newBaseline
+        $thisBaseline       = Get-OVBaseline -SppName $newBaseline
         if ($NULL -ne $thisBaseline)
         {
+
+            if (-not $thisTemplate.firmware.manageFirmware)
+            {
+
+                $thisTemplate.firmware.manageFirmware = $true
+
+            }
+
             $baselineUri    = $thisBaseline.uri
             $thisTemplate.firmware.firmwareBaselineUri = $baselineUri
-            Send-HPOVRequest -uri $thisTemplate.uri -body $thisTemplate -method PUT
+
+            # Uncomment the following to change the installation method
+            ## You can change the following value to "FirmwareOnly" for iSUT online firmware only, or
+            ## "FirmwareOnlyOfflineMode" for offline firmware only mode.
+            # $thisTemplate.firmware.firmwareInstallType = "FirmwareAndOSDrivers"
+
+            ## By default, firmware is not force installed.  Use this to force the re-installation and/or
+            ## downgrade of firmware.
+            # $thisTemplate.firmware.forceInstallFirmware = $true
+
+            ## Use the following setting to change the activation type.  Activation is rebooting the host
+            ## when needed to complete the installation of a component.  You can change the value to
+            ## "Scheduled" in order to set a schedule within the server profile.
+            # $thisTemplate.firmware.firmwareActivationType = "Immediate"
+
+            Save-OVServerProfileTemplate -InputObject | Wait-OVTaskComplete
+
         }
         else
         {
+
             -ForegroundColor YELLOW "New baseline --> $newBaseline does not exist. Skip modifying template..."
+
         }
 }
+
 else
 {
+
     write-host -ForegroundColor YELLOW "Template --> $template does not exist. Skip modifying it..."
+
+}
+
+
+# Get the list of servers profiles that are no longer compliant, and asynchronously update them
+
+$tasks = @()
+ForEach ($Server in (Get-OVServerProfile -NotCompliant))
+{
+
+    $tasks += Update-OVServerProfile -InputObject $Server -Async
+
 }
